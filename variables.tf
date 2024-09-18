c710c99af92c45a03b88b48365da6169c9c5091e
Delfin Alejandro Lopez Ricardi <delfinz23pretorian@gmail.com>
2024-09-18 14:42:18 -0400

    Add files via upload

---
+++ variables.tf
@@ -0,0 +1,26 @@
+variable "region" {
+  description = "The AWS region to deploy resources in"
+  default     = "us-east-2"
+}
+
+variable "vpc_cidr" {
+  description = "CIDR block for the VPC"
+  default     = "10.0.0.0/16"
+}
+
+variable "public_subnet_cidr_blocks" {
+  description = "CIDR blocks for the public subnets"
+  type        = list(string)
+  default     = ["10.0.1.0/24", "10.0.2.0/24"]
+}
+
+variable "private_subnet_cidr_blocks" {
+  description = "CIDR blocks for the private subnets"
+  type        = list(string)
+  default     = ["10.0.3.0/24", "10.0.4.0/24"]
+}
+
+variable "eks_cluster_name" {
+  description = "Name of the EKS cluster"
+  default     = "Solar-System-Cluster"
+}
